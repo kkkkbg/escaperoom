75d2a50cfbf4b1c16b81d691fb206fa152d400a9
최지은 <108658971+kkkkbg@users.noreply.github.com>
2022-12-19 10:33:48 +0900

    Update share.js

--- js/share.js
+++ js/share.js
@@ -1,11 +1,10 @@
-const url = 'https://before2pm.netlify.app/';
+const url = 'https://withmoon.netlify.app/';
 
 //카톡공유버튼 클릭했을 때 호출. 결과 화면에서 결과 번호를 받음
 function setShare(){
-    var resultImg = document.querySelector('#resultImg');
-    var resultAlt = resultImg.firstElementChild.alt;  //책번호
-    const shareTitle = '문제풀이 결과';
-    const shareDes = infoList[resultAlt].name;  //책이름
+    var count = document.querySelector('#count').val;  //틀린갯수
+    const shareTitle = '김신입 구하기 성공( •̀ ω •́ )✧';
+   // const shareDes = infoList[resultAlt].name;  //책이름
     const shareImage = url + 'img/key.jpg';
     const shareURL = url + 'index.html';  //이거 해줄려고 html 하나하나 만들었음,,
     
@@ -13,7 +12,7 @@ function setShare(){
         objectType: 'feed',
         content: {
           title: shareTitle,
-          description: '내 점수는 '+shareDes+'( •̀ ω •́ )✧',
+          description: '틀린 횟수 : '+count,
           imageUrl: shareImage,
           link: {
             mobileWebUrl: shareURL,
@@ -22,7 +21,7 @@ function setShare(){
         },
         buttons: [
           {
-            title: '나도 해볼래!',
+            title: '김신입을 구해주세요!',
             link: {
               mobileWebUrl: shareURL,
               webUrl: shareURL,
